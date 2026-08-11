70c295444fb7d2ef6753c3a630477b363ea77993
Max <kiporenkomaks8@gmail.com>
2026-08-11 18:32:20 +0300

    fixed permossions.py

--- user/tests/test_user_api.py
+++ user/tests/test_user_api.py
@@ -1,13 +1,14 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
+from rest_framework.reverse import reverse_lazy
 
 from rest_framework.test import APIClient
 from rest_framework import status
 
 
 CREATE_USER_URL = reverse("user:create")
-TOKEN_URL = reverse("user:login")
+TOKEN_URL = reverse_lazy("user:token")
 ME_URL = reverse("user:manage")
 
 
